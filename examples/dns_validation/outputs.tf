be960453c00ca0530ecafa14fbf8d0d5669be383
Nur Adhi N <nur.nugroho@traveloka.com>
2020-08-31 12:36:19 +0700

    Support Terraform v0.12 (#26)

--- examples/dns_validation/outputs.tf
+++ examples/dns_validation/outputs.tf
@@ -1,7 +1,8 @@
 output "certificate_arn" {
-  value = "${module.acm.acm_certificate_arn}"
+  value = module.acm.acm_certificate_arn
 }
 
 output "validation_record" {
-  value = "${module.acm.acm_certificate_dns_validation_record}"
+  value = module.acm.acm_certificate_dns_validation_record
 }
+
