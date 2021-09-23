227a7cc40f770682ff5b39eec9b38589aa1fb0bb
falihrafi <falihrafi@gmail.com>
2021-09-23 11:47:06 +0700

    (2) do small changes in examples/dns_validation/main.tf file

--- examples/dns_validation/main.tf
+++ examples/dns_validation/main.tf
@@ -3,7 +3,7 @@ module "acm" {
   domain_name      = "*.tsi.dev-tvlk.cloud"
   hosted_zone_name = "tsi.dev-tvlk.cloud"
   certificate_name = "wildcard.tsi.dev-tvlk.cloud"
-  environment      = "tetsting"
+  environment      = "testing"
   description      = "Wildcard certificate for example.tsi.dev-tvlk.cloud"
   product_domain   = "exm"
 }
